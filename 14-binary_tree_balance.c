8ea4f0ef773425d491d78c92020bee8b2ed5b781
Titania792 <darkwarrior792@gmail.com>
2022-07-17 12:25:24 -0700

    trying to fix task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,28 +11,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (-1);
-	if (binary_tree_is_leaf(tree))
-		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	if (left_height > right_height)
 		return (left_height + 1);
 	else
 		return (right_height + 1);
-}
-
-/**
- * binary_tree_is_leaf - checks if a node is a leaf
- * @node: node to check
- * Return: 1 if node is a leaf, otherwise 0
- */
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node == NULL)
-		return (0);
-	if (node->left == NULL && node->right == NULL)
-		return (1);
-	return (0);
 }
 
 /**
