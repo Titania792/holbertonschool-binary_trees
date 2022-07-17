f87004535c81cb1ae46ffedb5f905bcd8551a326
Titania792 <darkwarrior792@gmail.com>
2022-07-17 12:47:24 -0700

    second try task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,16 +7,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0, right_height = 0;
 
 	if (!tree)
 		return (-1);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+	if (tree->left)
+		left_height = binary_tree_height(tree->left) + 1;
+	if(tree->right)
+		right_height = binary_tree_height(tree->right) + 1;
+	return (left_height > right_height ? left_height : right_height);
 }
 
 /**
