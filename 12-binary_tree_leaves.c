b60e118010837f082096e485fe4df68af39f7ac6
Dak4rPrO <4382@holbertonschool.com>
2022-07-18 16:11:46 -0700

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -15,6 +15,5 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right  == NULL)
 		return (1);
-	return (binary_tree_leaves(tree->left));
-	return (binary_tree_leaves(tree->right));
+	return ((binary_tree_leaves(tree->left)) + (binary_tree_leaves(tree->right)));
 }
